b3026271edbbed495d443dce3979e89122985c37
user <user@Kings-MacBook-Pro.local>
2020-02-12 14:31:36 -0600

    updated

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -5,11 +5,17 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GreetingController {
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private GreetingService service;
@@ -42,6 +48,16 @@ public class GreetingController {
 	@GetMapping("/dbSettings")
 	public String getDbSettings() {
 		return dbSettings.getConnection() + " " + dbSettings.getHost() + " " + dbSettings.getPort();
+	}
+	
+	@GetMapping("/env")
+	public String getAllEnvironmentDetail() {
+		return env.toString();
+	}
+
+	@GetMapping("/envDetails")
+	public String getEnvDetails() {
+		return null;
 	}
 
 }
